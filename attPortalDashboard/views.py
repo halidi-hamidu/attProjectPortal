e8719cb5a766ae7e1fa260390a793ce087918493
HALIDY <halidikijuru109@gmail.com>
2023-06-15 16:12:37 +0300

    Layout changed

--- attPortalDashboard/views.py
+++ attPortalDashboard/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render, redirect
 from attPortalMember.forms import AttPortalMemberModelForm
 from django.contrib import messages
+from attPortalMember.models import AttPortalMemberModel
 # Create your views here.
 def attPortalDashboardPage(request):
-    if request.method == "POST":
-        form = AttPortalMemberModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, " Member added successful")
-            return redirect("attPortalDashboard:attPortalDashboardPage")
+    try:
+        if request.method == "POST":
+            form = AttPortalMemberModelForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, " Member added successful")
+                return redirect("attPortalDashboard:attPortalDashboardPage")
+    except:
+        return redirect("attPortalDashboard:attPortalDashboardPage")
+    get_all_member= AttPortalMemberModel.objects.all().order_by('-created_at')
+    count_all_member= AttPortalMemberModel.objects.all().count
     form = AttPortalMemberModelForm()
     template_name = 'attPortalDashboard/attPortalDashboardPage.html'
     context = {
-        'form':form
+        'form':form,
+        'get_all_member':get_all_member,
+        'count_all_member':count_all_member
     }
 
     return render (request, template_name, context)
+
+    
